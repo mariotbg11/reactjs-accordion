97b12341efbc23144c07b10450f363e5862db522
mariotbg11 <christofell.m@gmail.com>
2024-01-23 15:13:31 +0700

    update readme and add footer component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import Accordion from "./components/Accordion";
+import Footer from "./components/Footer";
 import faqs from "./datafaqs";
 
 function App() {
   return (
     <div className="container">
       <Accordion data={faqs} />
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,17 @@
+function Footer() {
+  return (
+    <div className="footer">
+      Challenge by{" "}
+      <a
+        href="https://www.frontendmentor.io?ref=challenge"
+        target="_blank"
+        rel="noreferrer"
+      >
+        Frontend Mentor
+      </a>
+      . Coded by <a href="https://github.com/mariotbg11">Mario Christofell</a>.
+    </div>
+  );
+}
+
+export default Footer;
